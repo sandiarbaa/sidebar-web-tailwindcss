da325b3324cbabc6c2b87c55d7e4a68f76303089
sandiarbaa <sandiarbaa@gmail.com>
2024-01-23 06:22:28 +0700

    project done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./pages/about.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
   },
   plugins: [],
 };
